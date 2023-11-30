e79c281e4c192f5ff7ae55a328d3b68a2a69fc29
codepanther <pezechigozirim@gmail.com>
2023-11-30 10:01:10 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,31 @@
+require('dotenv').config();
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+export const app = express();
+
+// body parser
+app.use(express.json({ limit: '50mb' }));
+// cookie parser
+app.use(cookieParser());
+// cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN
+  })
+);
+
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: 'API is working'
+  });
+});
+
+// unknown route middleware
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} does not exist.`) as any;
+  err.statusCode = 404;
+  next(err);
+})
