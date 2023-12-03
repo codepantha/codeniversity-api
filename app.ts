f0c55bb59191512a2e790f988ee5bd5dff7c74b9
codepanther <pezechigozirim@gmail.com>
2023-12-03 04:40:06 +0100

    Add errorHanlder middleware

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+import ErrorMiddleware from './middleware/error';
+
 export const app = express();
 
 // body parser
@@ -28,4 +30,6 @@ app.all('*', (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} does not exist.`) as any;
   err.statusCode = 404;
   next(err);
-})
+});
+
+app.use(ErrorMiddleware);
